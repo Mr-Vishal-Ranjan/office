f423f6fa7070161e84d84e90005d9c25d591532c
vishal <vr842002@gmail.com>
2024-02-13 14:55:06 +0530

    React

---
+++ ReactApp/test/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
+
+export default function App(){
+  const listItems = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {product.title}
+    </li>
+  );
+
+  return (
+    <ul>{listItems}</ul>
+  );
+  // return (
+  //   <div>
+  //   <h1>{user.name}</h1>
+  //   <img src={user.imageUrl} alt={user.name} style={{
+  //         padding:10,borderRadius:50,
+  //       }}/>
+  //   </div>
+  // );
+}
